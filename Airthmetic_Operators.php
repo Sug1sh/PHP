07a08f56f0c87a2b7e1972e28f11361a0b0d371d
Sugish Devkota <inventyourshit7@gmail.com>
2026-03-11 06:07:00 -0400

    Add files via upload

---
+++ Airthmetic_Operators.php
@@ -0,0 +1,14 @@
+<?php
+/*Arithmetic operators
+  + - * / %                        */
+
+
+$a = 10;
+$b = 40;
+$c = null;
+
+$c = $a + $b;
+echo "The Sum of Two Numbers is: {$c} <br>";
+print('we are currently learning php');
+
+?>
