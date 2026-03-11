3c8b224c42911b57bf5a3680eaa9f6239b4eb18b
Sugish Devkota <inventyourshit7@gmail.com>
2026-03-11 06:08:04 -0400

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,26 @@
+<?php
+$name = "Sug1sh.";
+$me = "Security Researcher.";
+$Email = "sugishdevkota17@gmail.com";
+$salary = 200;
+$size = 0;
+$size1 = 1;
+$size2 = 2;
+$size3 = 3;
+$total = null;
+
+echo "My Name is {$name}<br>";
+echo "I love to be a {$me} <br>";
+echo "My Email Address is {$Email}<br><br><br><br>";
+echo " Congratulations {$name}The Bounty you've Received is $ {$salary}.<br>";
+$total = $salary*$size2;
+echo"The Total money is $ {$total}<br>";
+
+
+
+
+
+
+
+
+
